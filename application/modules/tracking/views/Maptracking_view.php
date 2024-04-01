68f5553962c1c08fc5431d7b2fd9af0c4d93d5ea
Milkanzs23 <nzosabamilka@gmail.com>
2024-04-01 16:32:44 +0100

    tracking chauffeur update

--- application/modules/tracking/views/Maptracking_view.php
+++ application/modules/tracking/views/Maptracking_view.php
@@ -486,24 +486,24 @@ z-index: 100;
 
 });
 
-    function toggleSidebar(id) {
-      const elem = document.getElementById(id);
-        // Add or remove the 'collapsed' CSS class from the sidebar element.
-        // Returns boolean "true" or "false" whether 'collapsed' is in the class list.
-      const collapsed = elem.classList.toggle('collapsed');
-      const padding = {};
-        // 'id' is 'right' or 'left'. When run at start, this object looks like: '{left: 300}';
-        padding[id] = collapsed ? 0 : 300; // 0 if collapsed, 300 px if not. This matches the width of the sidebars in the .sidebar CSS class.
-        // Use `map.easeTo()` with a padding option to adjust the map's center accounting for the position of sidebars.
-        map_map.easeTo({
-          padding: padding,
-            duration: 1000 // In ms. This matches the CSS transition duration property.
-          });
-      }
+    // function toggleSidebar(id) {
+    //   const elem = document.getElementById(id);
+    //     // Add or remove the 'collapsed' CSS class from the sidebar element.
+    //     // Returns boolean "true" or "false" whether 'collapsed' is in the class list.
+    //   const collapsed = elem.classList.toggle('collapsed');
+    //   const padding = {};
+    //     // 'id' is 'right' or 'left'. When run at start, this object looks like: '{left: 300}';
+    //     padding[id] = collapsed ? 0 : 300; // 0 if collapsed, 300 px if not. This matches the width of the sidebars in the .sidebar CSS class.
+    //     // Use `map.easeTo()` with a padding option to adjust the map's center accounting for the position of sidebars.
+    //     map_map.easeTo({
+    //       padding: padding,
+    //         duration: 1000 // In ms. This matches the CSS transition duration property.
+    //       });
+    //   }
 
 
       map_map.on('load', () => {
-        toggleSidebar('left');
+        // toggleSidebar('left');
 
         var donn='<?= $mark_vprim ?>';
 
